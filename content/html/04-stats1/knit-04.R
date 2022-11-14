f753981f5b1286bd45db6676f9cd11a1c796e7c4
colebrookson <cole.brookson@gmail.com>
2022-11-14 06:03:26 -0500

    some fixes

--- content/html/04-stats1/knit-04.R
+++ content/html/04-stats1/knit-04.R
@@ -2,9 +2,11 @@ library(knitr)
 library(here)
 
 setwd(here("./content/html/04-stats1"))
-dir <- "."
+dir <- "./"
 
 # knit R markdown to markdown
 files_04 <- list.files(dir, "\\.rhtml")
-for (f in files_04)
-  knit(f)
+for (f in files_04){
+  knit(f, envir = new.env())
+}
+
