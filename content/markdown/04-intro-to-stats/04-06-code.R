20c9250aa26c5cdb7a1bf1fc910d9beefbfc9015
Cole Brookson <cole.brookson@gmail.com>
2023-01-24 21:06:36 -0500

    code

--- content/markdown/04-intro-to-stats/04-06-code.R
+++ content/markdown/04-intro-to-stats/04-06-code.R
@@ -30,15 +30,7 @@ south_compare <- crab_sizes_south %>%
   dplyr::select(size) %>% 
   dplyr::mutate(region = "south")
 
-crab_compare <- rbind(
-  crab_sizes_north %>% 
-    dplyr::select(size) %>% 
-    dplyr::mutate(region = "north"),
-  crab_sizes_south %>% 
-    dplyr::select(size) %>% 
-    dplyr::mutate(region = "south")
-) %>% 
-  dplyr::mutate(region = as.factor(region))
+crab_compare <- rbind(north_compare, south_compare) 
 
 ggplot(data = crab_compare) + 
   geom_point(aes(x = region, y = size)) + 
@@ -48,7 +40,8 @@ ggplot(data = crab_compare) +
   geom_boxplot(aes(x = region, y = size)) + 
   ggthemes::theme_base()
 
-
+sd(south_compare$size)
+sd(north_compare$size)
 
 
 
@@ -61,3 +54,39 @@ t_test_res <- stats::t.test(
   var.equal = TRUE
 )
 t_test_res
+
+
+
+library(palmerpenguins)
+
+df <- palmerpenguins::penguins
+library(ggpubr)
+
+# first filter the data
+adelie <- df %>% 
+  dplyr::filter(species == "Adelie")
+chin <- df %>% 
+  dplyr::filter(species == "Chinstrap")
+gentoo <- df %>% 
+  dplyr::filter(species == "Gentoo")
+
+# plot qq-plots for each one
+ggpubr::ggqqplot(adelie$bill_length_mm, main = "Adelie")
+ggpubr::ggqqplot(chin$bill_length_mm, main = "Chinstrap")
+ggpubr::ggqqplot(gentoo$bill_length_mm, main = "Gentoo")
+
+ggplot(data = gentoo) + 
+  geom_density(aes(x = bill_length_mm))
+ggplot(data = chin) + 
+  geom_density(aes(x = bill_length_mm))
+ggplot(data = adelie) + 
+  geom_density(aes(x = bill_length_mm))
+
+
+
+stats::shapiro.test(adelie$bill_length_mm)
+stats::shapiro.test(chin$bill_length_mm)
+stats::shapiro.test(gentoo$bill_length_mm)
+
+
+
